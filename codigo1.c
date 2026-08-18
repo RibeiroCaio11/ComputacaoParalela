d6fdf8238f82aed896481b0b8b9f8657aa1775bd
Caio Ribeiro <colribeiro05@gmail.com>
2026-08-18 01:37:38 +0000

    kopdaskopasd

---
+++ codigo1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <pthread.h>
+#define NUM_THREADS 4
+#define N 10000000
+int *v;
+
+long long total = 0;
+
+void *soma_trecho(void *arg) {
+    long tid = (long) arg;
+    long ini = tid * (N / NUM_THREADS);
+    long fim = (tid + 1) * (N / NUM_THREADS);
+    for (long i = ini; i < fim; i++){
+        total += v[i]; // <<< CONDICAO DE CORRIDA
+     }
+return NULL;
+}
+
+
+int main(void){
+    v = malloc(N*sizeof(int));
+    pthread_t threads[NUM_THREADS];
+    for(int i = 0; i<N; i++){
+        v[i] = 10;
+
+    }
+    for( long t = 0; t<NUM_THREADS;t++){
+    
+    pthread_create(&threads[t], NULL, soma_trecho, (void *)t);
+    }
+    for( long t = 0; t<NUM_THREADS;t++){
+        pthread_join(threads[t],NULL);
+    }
+    printf("soma esperada:%lld\n",(long long) N*10);
+    printf("soma calculada:%lld\n",total);
+}
